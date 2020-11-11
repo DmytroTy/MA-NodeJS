16ae8552609c74304fe90355607b67ac41645051
Dmytro Tyshchenko <kwazar2008@ukr.net>
2020-11-11 18:19:25 +0200

    item 11

--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 const { task1: filterGoods, task2: goodWithMaxCost, task3 } = require('./task');
-const local = require('../goods');
 
-let store = [];
-let goods = local;
+const pathToFile = path.resolve(__dirname, '../', 'goods.json');
+
+function readFile() {
+  const rawdata = fs.readFileSync(pathToFile, 'utf8');
+  let goods;
+  try {
+    goods = JSON.parse(rawdata);
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  return goods;
+}
 
 function good(response, queryParams) {
-  const data = filterGoods(goods, queryParams.parameter, queryParams.value);
-  response.write(JSON.stringify(data));
+  const goods = filterGoods(readFile(), queryParams.parameter, queryParams.value);
+  response.write(JSON.stringify(goods));
   response.end();
 }
 
@@ -16,22 +28,15 @@ function findGoodWithMaxCost(response) {
 }
 
 function standardize(response) {
-  const standard = task3(goods);
+  const standard = task3(readFile());
   response.write(JSON.stringify(standard));
   response.end();
 }
 
 function newData(data, response) {
-  store = data;
-  response.write(JSON.stringify(store));
+  fs.writeFileSync(pathToFile, JSON.stringify(data, null, 1));
+  response.write(JSON.stringify(data));
   response.end();
 }
 
-function change(response, queryParams) {
-  if (queryParams.storage === 'store') goods = store;
-  if (queryParams.storage === 'json') goods = local;
-  response.write(JSON.stringify(goods));
-  response.end();
-}
-
-module.exports = { good, goodWithMaxCost: findGoodWithMaxCost, standardize, newData, change };
+module.exports = { good, goodWithMaxCost: findGoodWithMaxCost, standardize, newData };
